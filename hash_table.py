3a479fef819521cff77c64795e3980414cc67d8f
doorlay <nic@doorlay.com>
2020-03-05 17:48:21 -0800

    worked on funcs

---
+++ hash_table.py
@@ -0,0 +1,153 @@
+class HashTable:
+
+    def __init__(self, table_size):
+        self.table_size = table_size
+        self.hash_table = [None]*table_size
+        self.num_items = 0 
+
+    # I believe I have fixed both of these insert functions to work with my new implementation, but I may be wrong.
+
+    def insert(self, key):
+        """ Inserts an entry into the hash table (using Horner hash function to determine index, 
+        and quadratic probing to resolve collisions).
+        The key is a string (a word) to be entered, and value is any object (e.g. Python List).
+        If the key is not already in the table, the key is inserted along with the associated value
+        If the key is is in the table, the new value replaces the existing value.
+        When used with the concordance, value is a Python List of line numbers.
+        If load factor is greater than 0.5 after an insertion, hash table size should be increased (doubled + 1)."""
+
+        hash_value = self.horner_hash(key)
+        original_hash_value = hash_value
+        i = 1
+        while True:
+            hash_value = hash_value % self.table_size
+            if self.hash_table[hash_value] == None:
+                self.hash_table[hash_value] = (key, 1)
+                self.num_items += 1
+                load_factor = self.get_load_factor()
+                if load_factor > (1/2):
+                    old_hash = self.hash_table
+                    self.table_size = (self.table_size * 2) + 1
+                    self.hash_table = [None] * self.table_size
+                    for item in old_hash:
+                        if item != None:
+                            new_hash_value = self.horner_hash(item[0])
+                            self.rehash(new_hash_value,item)
+                break
+            elif self.hash_table[hash_value][0] == key:
+                new_value = self.hash_table[hash_value][1] + 1
+                self.hash_table[hash_value] = (key, new_value)
+            elif i == self.table_size:
+                break
+            hash_value = original_hash_value + (i ** 2)
+            i += 1  
+
+    def rehash(self,hash_value,item):
+        """ Function to rehash all items when the table size is increased, accounting for collisions."""
+
+        original_hash_value = hash_value
+        i = 1
+        while True:
+            hash_value = hash_value % self.table_size
+            if self.hash_table[hash_value] == None:
+                self.hash_table[hash_value] = item
+                break
+            elif self.hash_table[hash_value][0] == item[0]:
+                new_value = self.hash_table[hash_value][1] + 1
+                new_key = item[0]
+                item = (new_key, new_value)
+                self.hash_table[hash_value] = item
+            elif i == self.table_size:
+                break
+            hash_value = original_hash_value + (i ** 2)
+            i += 1
+            
+    def horner_hash(self, key):
+        """ Compute and return an integer from 0 to the (size of the hash table) - 1
+        Compute the hash value by using Horner’s rule, as described in project specification."""
+
+        h = 0
+        n = min(8, len(key))
+        for i in range(n):
+            h = (31*h)+ord(key[i])
+        return h % self.table_size
+
+    def in_table(self, key):
+        """ Returns True if key is in an entry of the hash table, False otherwise. Must be O(1)."""
+
+        hash_value = self.horner_hash(key)
+        original_hash_value = hash_value
+        i = 1
+        while True:
+            if hash_value > self.table_size - 1:
+                hash_value = hash_value % self.table_size
+            if self.hash_table[hash_value] == None:
+                return False
+            elif self.hash_table[hash_value][0] == key:
+                return True
+            elif i == self.table_size:
+                return False
+            hash_value = original_hash_value + (i ** 2)
+            i += 1  
+
+    def get_index(self, key):
+        """ Returns the index of the hash table entry containing the provided key. 
+        If there is not an entry with the provided key, returns None. Must be O(1)."""
+
+        hash_value = self.horner_hash(key)
+        original_hash_value = hash_value
+        i = 1
+        while True:
+            if hash_value > self.table_size - 1:
+                hash_value = hash_value % self.table_size
+            if self.hash_table[hash_value] == None:
+                return None
+            elif self.hash_table[hash_value][0] == key:
+                return hash_value
+            elif i == self.table_size:
+                return None
+            hash_value = original_hash_value + (i ** 2)
+            i += 1  
+
+    def get_all_keys(self):
+        """ Returns a Python list of all keys in the hash table."""
+
+        new_lst = []
+        for item in self.hash_table:
+            if item != None:
+                new_lst.append(item[0])
+        return new_lst
+
+    def get_value(self, key):
+        """ Returns the value (for concordance, list of line numbers) associated with the key.
+        If key is not in hash table, returns None. Must be O(1)."""
+
+        hash_value = self.horner_hash(key)
+        original_hash_value = hash_value
+        i = 1
+        while True:
+            if hash_value > self.table_size - 1:
+                hash_value = hash_value % self.table_size
+            if self.hash_table[hash_value] == None:
+                return None
+            elif self.hash_table[hash_value][0] == key:
+                return self.hash_table[hash_value][1]
+            elif i == self.table_size:
+                return None
+            hash_value = original_hash_value + (i ** 2)
+            i += 1  
+
+    def get_num_items(self):
+        """ Returns the number of entries (words) in the table. Must be O(1)."""
+
+        return self.num_items
+
+    def get_table_size(self):
+        """ Returns the size of the hash table."""
+
+        return self.table_size
+
+    def get_load_factor(self):
+        """ Returns the load factor of the hash table (entries / table_size)."""
+
+        return self.num_items / self.table_size
--- zipf.py
+++ zipf.py
@@ -1,5 +1,50 @@
-# main function for the program
+from hash_table import *
+import string
 
-def get_words(in_file):
-    input_file = open(in_file, "r")
-    read_input = input_file.readlines()
+def get_word_frequencies(filename):
+    """ Read words from input text file (filename) and insert them into the concordance hash table, 
+    after processing for punctuation, numbers and filtering out words that are in the stop words hash table.
+    Do not include duplicate line numbers (word appearing on same line more than once, just one entry for that line)
+    Starting size of hash table should be 191: self.concordance_table = HashTable(191)
+    Process of adding new line numbers for a word (key) in the concordance:
+    If word is in table, get current value (list of line numbers), append new line number, insert (key, value)
+    If word is not in table, insert (key, value), where value is a Python List with the line number
+    If file does not exist, raise FileNotFoundError"""
+
+    hsh = HashTable(191)
+    try:
+        f = open(filename, "r")
+    except:
+        raise FileNotFoundError
+    for line in f:
+        new_word = ""
+        for character in line:
+            if (ord(character) >= 65 and ord(character) <= 90) or (ord(character) >= 97 and ord(character) <= 122):
+                new_word += character
+            elif ord(character) == 39:
+                pass
+            else:
+                new_word = new_word.lower()
+                if new_word != "":
+                    # i need to check here if the words are already in the table. Do I though?
+                    hsh.insert(new_word)
+                new_word = ""
+    return hsh
+
+h = get_word_frequencies("lotr.txt")
+print('test')
+
+
+"""
+1. It appears that there are currently a lot of duplicate words in the table, so yes, I do need to be checking for dups before I add.
+2. Additionally, a lot of random single letters are being added, such as "t", "d", "s", and so on.
+3. Make sure I am accounting for punctuation correctly with this implementation.
+
+"""
+
+# I'm going to keep a hash table of every word that appears, and using that hash table I'm going to map everything out and plot word frequency and such.
+
+# hash table has a key and a value. 
+
+# the key will be the word, the value will be the frequency of that word. They will be stored in tuple? Is that efficient? not really.
+# ex. ('cat', 14)
