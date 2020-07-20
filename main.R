57c632c6c9c604a7eb982062590d0bd6ea4460d4
Nic Doorlay <n.doorlay@gmail.com>
2020-07-19 20:01:16 -0700

    Delete main.R

--- main.R
+++
@@ -1,7 +0,0 @@
-library(reticulate)
-os <- import("os")
-os$listdir(".")
-
-source_python("zipf.py")
-python_nested_list <- main()
-# This nested list must be reassigned to some form of data that works better with R's graphing
